65570064fcf8193ca43bff6ccbb66211a7cd2d8a
rutcio <mikalaiyel@gmail.com>
2023-06-30 14:11:17 +0300

    XD

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,21 +1,15 @@
+import Swiper from "https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.esm.browser.min.js";
+
 const swiper = new Swiper('.swiper', {
     // Optional parameters
-    direction: 'vertical',
+    direction: 'horizontal',
     loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
   
     // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-});
+  });
+
+export default swiper;
